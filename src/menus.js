86037a56f3823ee880924bd73513570c7a933745
arte-trimbauser <arteompotrimba71@gmail.com>
2026-02-22 20:13:10 +0000

    Update menus.js

--- src/menus.js
+++ src/menus.js
@@ -2,33 +2,93 @@ module.exports = [
   {
     id: "1415055590014849034",
     title: "🦈 Shark Menu 🦈",
-    embedDesc: "Semanal: €18\nMensal: €25\nTrimensal: €38\nLifetime: €63\n\n**SHARKMENU - FIVEM**\n🦈 Executor Incluso.\n🦈 Spoof unban global incluso.\n🦈 Executor e menu com uma interface moderna.\n🦈 O executor inclui stopper, dumper, ESP, aimbot e executor de código .lua.\n🦈 Menu totalmente externo, não aparece na captura de tela.\n🦈 Puxando arma em Likizão, Mqcu & Thunder.\n🦈 Puxando veículo em Likizão, Mqcu & Thunder.\n🦈 Removendo o Modo Novato de todas as cidades.\n🦈 Removendo a Tela de Morte de todas as cidades.\n🦈 Removendo WhiteList de FusionGroup, LotusGroup, SpaceGroup & SantaGroup.\n🦈 Bloqueando Tptome, Tpto, Segurar no H e tudo mais.\n🦈 Funções troll para se divertir com o menu.\n🦈 Funções pra crashar o jogador, congelar, bugar e explodir sem tomar log de kill.\n🦈 Interface do menu totalmente moderna.\n🦈 Menu totalmente otimizado.",
+    embedDesc: `Semanal: €18  
+Mensal: €25  
+Trimensal: €38  
+Lifetime: €63  
+
+**SHARKMENU - FIVEM**
+🦈 Executor Incluso.
+🦈 Spoof unban global incluso.
+🦈 Executor e menu com uma interface moderna.
+🦈 O executor inclui stopper, dumper, ESP, aimbot e executor de código .lua.
+🦈 Menu totalmente externo, não aparece na captura de tela.
+🦈 Puxando arma em Likizão, Mqcu & Thunder.
+🦈 Puxando veículo em Likizão, Mqcu & Thunder.
+🦈 Removendo o Modo Novato de todas as cidades.
+🦈 Removendo a Tela de Morte de todas as cidades.
+🦈 Removendo WhiteList de FusionGroup, LotusGroup, SpaceGroup & SantaGroup.
+🦈 Bloqueando Tptome, Tpto, Segurar no H e tudo mais.
+🦈 Funções troll para se divertir com o menu.
+🦈 Funções pra crashar o jogador, congelar, bugar e explodir sem tomar log de kill.
+🦈 Interface do menu totalmente moderna.
+🦈 Menu totalmente otimizado.`,
     options: [
       { label: "Shark Semanal", description: "Semanal: €18", value: "shark_semanal" },
       { label: "Shark Mensal", description: "Mensal: €25", value: "shark_mensal" },
       { label: "Shark Trimensal", description: "Trimensal: €38", value: "shark_trimensal" },
       { label: "Shark Lifetime", description: "Lifetime: €63", value: "shark_lifetime" }
     ],
-    ticketMessage: (tipo) => "obrigado(a) por criar um ticket, em breve algum staff te ajudara",
+    ticketMessage: (tipo) => `obrigado(a) por criar um ticket, em breve algum staff te ajudara`,
     embedImage: "https://i.postimg.cc/htfrmgqR/image.png"
   },
   {
     id: "1415056008845332550",
     title: "🦍 Stan Menu 🦍",
-    embedDesc: "Diario: €3,5\nSemanal: €6,5\nMensal: €13\nLifetime: €40,5\n\nSTAN MENU 2.2\n💣・Puxar armas nos principais servidores! Mqcu/Likizao\n💣・Menu com interface Nova e otimizada!\n💣・Virando Skins Em Varias Cidades\n💣・Remove modo novato.\n💣・Diversas funções trolls.\n💣・Silent Aim\n💣・Aimbot\n💣・Puxando Players MQCU/likzao\n💣・Esp Externo No Executor\n💣・Skins Trolls MQCU/Likizao\n💣・Super Soco.\n💣・Grudar No Cavalinho No Player Ou X3.\n💣・Sistema de dar ADV nos carros tecla bind.\n💣・Pulando Whitelist em TODAS cidades.",
+    embedDesc: `
+Diario: €3,5
+Semanal: €6,5
+Mensal: €13
+Lifetime: €40,5
+STAN MENU 2.2
+💣・Puxar armas nos principais servidores! Mqcu/Likizao
+💣・Menu com interface Nova e otimizada!
+💣・Virando Skins Em Varias Cidades
+💣・Remove modo novato.
+💣・Diversas funções trolls.
+💣・Silent Aim
+💣・Aimbot
+💣・Puxando Players MQCU/likzao
+💣・Esp Externo No Executor
+💣・Skins Trolls MQCU/Likizao
+💣・Super Soco.
+💣・Grudar No Cavalinho No Player Ou X3.
+💣・Sistema de dar ADV nos carros tecla bind. LotusGroup/Nexus/FusionGroup
+💣・Pulando Whitelist em TODAS cidades, LotusGroup/SantasGroup`,
     options: [
       { label: "Diario", description: "Diario: €3,5", value: "stan_diario" },
       { label: "Semanal", description: "Semanal: €6,5", value: "stan_semanal" },
       { label: "Mensal", description: "Mensal: €13", value: "stan_mensal" },
       { label: "Lifetime", description: "Lifetime: €40,5", value: "stan_lifetime" }
     ],
-    ticketMessage: (tipo) => "obrigado(a) por criar um ticket, em breve algum staff te ajudara",
+    ticketMessage: (tipo) => `obrigado(a) por criar um ticket, em breve algum staff te ajudara`,
     embedImage: "https://i.postimg.cc/PqJBN0Sy/image.png"
   },
   {
     id: "1415057511668514936",
     title: "⭐ Stellar Menu ⭐",
-    embedDesc: "Diario: €4\nSemanal: €6\nMensal: €10\nTrimensal: €15\nLifetime: €22\n\n**STELLARMENU - FIVEM**\n⭐ Puxar armas e carros Mqcu/Likizao/Fiveguard\n⭐ Menu com interface Nova e otimizada!\n⭐ Removendo modo novato e tela de morte\n⭐ Bugar Player / Chuva de Veiculos\n⭐ Silent Aim / Aimbot / RageBot / Esp\n⭐ STOPER E LUA EXECUTOR / SPOOFER Incluso",
+    embedDesc: `Diario:€4
+Semanal: €6
+Mensal: €10
+Trimensal: €15
+Lifetime: €22
+
+**STELLARMENU - FIVEM**
+⭐ Puxar armas nas principais Cidades! Mqcu/Likizao/Fiveguard
+⭐ Puxando carros nas principais Cidades! Mqcu/Likizao/Fiveguard
+⭐ Menu com interface Nova e otimizada!
+⭐ Removendo modo novato em varias cidades
+⭐ Removendo tela de morte em varias cidades
+⭐ Bugar Player
+⭐ Chuva de Veiculos
+⭐ Indexar veiculos
+⭐ Magneto
+⭐ Silent Aim
+⭐ Aimbot
+⭐ RageBot
+⭐ Esp
+⭐ STOPER E LUA EXECUTOR
+⭐ Incluso um SPOOFER para Banimentos Globais`,
     options: [
       { label: "Diario", description: "Diario: €4", value: "stellar_diario" },
       { label: "Semanal", description: "Semanal: €6", value: "stellar_semanal" },
@@ -36,122 +96,221 @@ module.exports = [
       { label: "Trimensal", description: "Trimensal: €15", value: "stellar_trimensal" },
       { label: "Lifetime", description: "Lifetime: €22", value: "stellar_lifetime" }
     ],
-    ticketMessage: (tipo) => "obrigado(a) por criar um ticket, em breve algum staff te ajudara",
+    ticketMessage: (tipo) => `obrigado(a) por criar um ticket, em breve algum staff te ajudara`,
     embedImage: ""
   },
   {
     id: "1397180703728210030",
     title: "🌙 LUNAX M3NU - OFICIAL 🌙",
-    embedDesc: "Diario: €2,50\nSemanal: €5\nMensal: €10\nLifetime: €25\n\n🔹・Suporte a MQCU, Likizão, Fiveguard.\n🔹・Crash funcional e Modo PED.\n🔹・Bypass completo e Spoofer incluso.\n🔹・Aimbot & ESP externo.",
+    embedDesc: `Diario: €2,50
+    Semanal: €5
+    Mensal: €10
+    Lifetime: €25
+    🔹・Compatível com as principais cidades – Total suporte a sistemas como MQCU, Likizão, Fiveguard e outros.
+    🔹・Menu leve e atualizado – Nova interface otimizada, fácil de usar e com design moderno.
+    🔹・Crash funcional – Derrube jogadores em diversas cidades com estabilidade.
+    🔹・Modo PED desbloqueado – Transforme-se em PED em várias cidades.
+    🔹・Bypass completo – Remove o modo novato automaticamente em qualquer cidade.
+    🔹・Aimbot & ESP externo – Visual completo dos inimigos + mira assistida, tudo sem executar nada pesado.
+    🔹・Free Cam com controle total – Visualize livremente o mapa com diferentes ângulos.
+    🔹・Olhos laser 100% funcionais – Visibilidade ampliada com compatibilidade ampla.
+    🔹・Pula whitelist – Funciona em LotusGroup, SantasGroup, FusionGroup e mais.
+    🔹・Spoofer incluso – Proteção contra banimentos globais já integrada no sistema.`,
     options: [
       { label: "Diario", description: "Diario: €2,50", value: "lunax_diario" },
       { label: "Semanal", description: "Semanal: €5", value: "lunax_semanal" },
       { label: "Mensal", description: "Mensal: €10", value: "lunax_mensal" },
       { label: "Lifetime", description: "Lifetime: €25", value: "lunax_lifetime" }
     ],
-    ticketMessage: (tipo) => "obrigado(a) por criar um ticket, em breve algum staff te ajudara",
+    ticketMessage: (tipo) => `obrigado(a) por criar um ticket, em breve algum staff te ajudara`,
     embedImage: "https://i.postimg.cc/6Q40Bn0H/imagem-2025-10-25-183842481.png"
   },
   {
     id: "1413602201842221077",
     title: "🟣 Flyside Menu 🟣",
-    embedDesc: "Semanal: €5\nMensal: €14\nLifetime: €30\n\n🚀 Flyside Cheat 🚀\n🎯 AimBot, Visuals, God Mode & More.",
+    embedDesc: `Semanal: €5
+    Mensal: €14
+    Lifetime: €30
+
+    🚀 Flyside Cheat 🚀
+    🎯 AimBot & Silent Aim:
+    ・Say goodbye to missing shots! With AimBot, every bullet hits its mark.
+    ・With Silent Aim, take down enemies without them even knowing!
+
+    👀 Visuals That Give You The Upper Hand:
+    ・ESP: See players, vehicles, and objects through walls and plan your moves ahead.
+    ・Free Camera: Explore the map like a true master, with total freedom to move anywhere.
+
+    💪 Player Modifications:
+    ・God Mode: Unstoppable! Become invincible and immune to damage.
+    ・Invisible Mode: Stay unseen and confuse everyone around you.
+    ・Infinite Stamina: No more running out of breath – sprint, jump, and perform actions endlessly.
+    ・Super Jump: Reach the impossible and surprise your enemies with insane jumps.
+    ・No Ragdoll & Explosive Melee: Fight like a beast with no limitations!
+
+    🚀 Teleportation & Movement:
+    ・Teleport To Point/Waypoint: Instantly teleport anywhere on the map – no more waiting around!
+    ・Player Speed & Auto Farm: Move faster than ever and let the game work for you.
+
+    🚗 Vehicle Features:
+    ・Bulletproof Tires & Invisible Vehicles: Protect your ride with bulletproof tires and stay off the radar.
+    ・Rocket Boost & Drift Mode: Boost your vehicle to insane speeds and drift like a pro.
+    ・Vehicle Spawner (Safe): Spawn any vehicle safely, without worry.
+    ・Repair & Max Fuel: Keep your vehicle in top condition with instant repairs and infinite fuel.
+
+    💥 Full Vehicle Control:
+    ・Kick All & Steal Vehicles: Take control by kicking others out or stealing their rides.
+    ・Explode & Explode All: Cause chaos with explosive power, targeting players or vehicles.
+
+    🔫 Weapon Spawner & Player List with Fun Troll Functions:
+    ・Weapon Spawner: Equip yourself with powerful weapons to dominate your enemies.`,
     options: [
       { label: "Semanal", description: "Semanal: €5", value: "flyside_semanal" },
       { label: "Mensal", description: "Mensal: €14", value: "flyside_mensal" },
       { label: "Lifetime", description: "Lifetime: €30", value: "flyside_lifetime" }
     ],
-    ticketMessage: (tipo) => "obrigado(a) por criar um ticket, em breve algum staff te ajudara"
+    ticketMessage: (tipo) => `obrigado(a) por criar um ticket, em breve algum staff te ajudara`
   },
   {
     id: "1393920033369096363",
     title: "🎮 Contas Rockstar - FiveM 🎮",
-    embedDesc: "1 Conta: €0.50\n・Full Acesso (Email/Senha)\n・Garantia de 5 dias\n・Entrega automática",
+    embedDesc: `1 Conta: €0.50
+    ・Full Acesso
+    ・Alterar Email e senha
+    ・Garantia de 5 dias caso caia ou não dê para entrar
+    ・Após a entrega não poderá ser efetuado o reembolso por se tratar de um produto irreversível
+    ・Entrega automática`,
     options: [
       { label: "Conta Rockstar", description: "1 Conta: €0.50", value: "rockstar_1conta" }
     ],
-    ticketMessage: (tipo) => "obrigado(a) por criar um ticket, em breve algum staff te ajudara",
+    ticketMessage: (tipo) => `obrigado(a) por criar um ticket, em breve algum staff te ajudara`,
     embedImage: "https://i.postimg.cc/8CTpSmNG/imagem-2025-10-25-184314535.png"
   },
   {
     id: "1413602383296200754",
     title: "💬 Contas Discord - FiveM 💬",
-    embedDesc: "10 Meses + Phone: €0.85\n3 Meses + Phone: €0.75\n30 Dias + Phone: €0.65\nConta 2023: €1.00",
+    embedDesc: `
+    💬 Contas Discord - FiveM 💬
+    Discord Account 10 Months + Phone Registered: €0.85
+    Discord Account 3 Months + Phone Registered: €0.75
+    Discord Account 30 days + Phone Registered: €0.65
+    Discord Account 2023: €1.00`,
     options: [
-      { label: "Discord 10 Meses", description: "€0.85", value: "discord_10m" },
-      { label: "Discord 3 Meses", description: "€0.75", value: "discord_3m" },
-      { label: "Discord 30 Dias", description: "€0.65", value: "discord_30d" },
+      { label: "Discord 10 Meses + Phone", description: "€0.85", value: "discord_10m" },
+      { label: "Discord 3 Meses + Phone", description: "€0.75", value: "discord_3m" },
+      { label: "Discord 30 Dias + Phone", description: "€0.65", value: "discord_30d" },
       { label: "Discord 2023", description: "€1.00", value: "discord_2023" }
     ],
-    ticketMessage: (tipo) => "obrigado(a) por criar um ticket, em breve algum staff te ajudara",
+    ticketMessage: (tipo) => `obrigado(a) por criar um ticket, em breve algum staff te ajudara`,
     embedImage: "https://i.postimg.cc/fbWMCHBd/imagem-2025-12-07-002708325.png"
   },
   {
     id: "1413601792138416258",
     title: "🔥 Contas Steam - FiveM 🔥",
-    embedDesc: "Steam Aged: €0.25\nSteam 100h FiveM: €2.00\nSteam Random Keys: €0.50",
+    embedDesc: `Steam Account Aged: €0.25
+    Steam Account 100h FiveM: €2.00
+    Steam Random Keys: €0.50`,
     options: [
       { label: "Steam Aged", description: "€0.25", value: "steam_aged" },
       { label: "Steam 100h FiveM", description: "€2.00", value: "steam_100h" },
       { label: "Steam Random Keys", description: "€0.50", value: "steam_random" }
     ],
-    ticketMessage: (tipo) => "obrigado(a) por criar um ticket, em breve algum staff te ajudara",
-    embedImage: "https://i.postimg.cc/dVwFQHMW/image.png"
+    ticketMessage: (tipo) => `obrigado(a) por criar um ticket, em breve algum staff te ajudara`,
+      embedImage: "https://i.postimg.cc/dVwFQHMW/image.png"
   },
   {
     id: "1414712954762432623",
     title: "🛡 Spoofer 1 Click 🛡",
-    embedDesc: "Diario: €2\nSemanal: €5\nMensal: €7\nLifetime: €25\n・Remove ban Global/HWID com 1 CLICK.",
+    embedDesc: `Diario: €2
+Semanal: €5
+Mensal: €7
+Lifetime: €25
+・Remove ban Global/HWID.
+・Remove o banimento com 1 CLICK
+・Suporte em call (Sem taxa).
+・Preço Justo e Acessível.`,
     options: [
       { label: "Diario", description: "€2", value: "spoofer1_diario" },
       { label: "Semanal", description: "€5", value: "spoofer1_semanal" },
       { label: "Mensal", description: "€7", value: "spoofer1_mensal" },
       { label: "Lifetime", description: "€25", value: "spoofer1_lifetime" }
     ],
-    ticketMessage: (tipo) => "obrigado(a) por criar um ticket, em breve algum staff te ajudara"
+    ticketMessage: (tipo) => `obrigado(a) por criar um ticket, em breve algum staff te ajudara`
   },
   {
     id: "1429835071199772752",
-    title: "🔥 Spoofer Permanente 🔥",
-    embedDesc: "Diario: €5\nSemanal: €10\nLifetime: €25\n✨ Suporta Valorant, FiveM, CoD, Rust, etc.",
+    title: "**🔥Spoofer Permanente🔥**",
+    embedDesc: `**Diario: €5
+Semanal: €10
+Mensal: €15
+Lifetime: €25
+✨ Jogos Triple A Suportados ✨
+🎮 VALORANT
+🎮 FORTNITE (Torneios seguros garantidos)
+🎮 APEX LEGENDS
+🎮 RUST
+🎮 RAINBOW SIX SIEGE
+🎮 OVERWATCH 2
+🎮 CALL OF DUTY (Inclui todos os títulos)
+🎮 FIVEM
+🎮 DAYZ
+
+🌟 E mais! Compatível com todos os jogos que utilizam EAC, BattleEye, Ricochet, e Vanguard.
+
+✅ Resumindo: Compatibilidade garantida com TODOS os jogos! 🎉
+
+Prepare-se para a melhor experiência e performance em todos os seus títulos favoritos! 🔥
+
+APÓS O ENVIO DA KEY E O RESGATE NÃO SERÁ POSSIVELO REEMBOLSO!**`,
     options: [
-      { label: "Diario", description: "€5", value: "spooferperm_diario" },
-      { label: "Semanal", description: "€10", value: "spooferperm_semanal" },
-      { label: "Lifetime", description: "€25", value: "spooferperm_lifetime" }
+      { label: "Diario", description: "€5", value: "spooferpermanente_diario" },
+      { label: "Semanal", description: "€10", value: "spooferpermanente_semanal" },
+      { label: "Mensal", description: "€15", value: "spooferpermanente_mensal" },
+      { label: "Lifetime", description: "€25", value: "spooferpermanente_lifetime" }
     ],
-    ticketMessage: (tipo) => "obrigado(a) por criar um ticket, em breve algum staff te ajudara"
+    ticketMessage: (tipo) => `obrigado(a) por criar um ticket, em breve algum staff te ajudara`
   },
   {
     id: "1413602908863205388",
     title: "🔒 VPN Service 🔒",
-    embedDesc: "Cyber Ghost: €3.00\nTunnel Bear: €4.00\nIPVanish: €1.00",
+    embedDesc: `Cyber Ghost VPN (Bypass Fiveguard): €3.00
+Tunnel Bear VPN: €4.00
+IPVanish VPN: €1.00`,
     options: [
-      { label: "Cyber Ghost", description: "€3.00", value: "vpn_cyber" },
-      { label: "Tunnel Bear", description: "€4.00", value: "vpn_tunnel" },
+      { label: "Cyber Ghost (Bypass)", description: "€3.00", value: "vpn_cyberghost" },
+      { label: "Tunnel Bear", description: "€4.00", value: "vpn_tunnelbear" },
       { label: "IPVanish", description: "€1.00", value: "vpn_ipvanish" }
     ],
-    ticketMessage: (tipo) => "obrigado(a) por criar um ticket, em breve algum staff te ajudara",
+    ticketMessage: (tipo) => `obrigado(a) por criar um ticket, em breve algum staff te ajudara`,
     embedImage: "https://i.postimg.cc/jdr0vgjt/image.png"
   },
   {
     id: "1414922861239337071",
     title: "🦆 Duck Cleaner 🦆",
-    embedDesc: "€1\nRemoção automática de pastas para spoof.",
+    embedDesc: `€1
+    ・Cansado de fazer sempre o mesmo processo em antes de spoofar?
+    ・DUCK CLEANER E A TUA SOLUCAO, REMOVE TODAS AS PASTAS QUE PRECISAS APAGAR AUTOMATICAMENTE!
+    ・Suporte em call (Sem taxa).
+    ・Preço Justo e Acessível.`,
     options: [
       { label: "Duck Cleaner", description: "€1", value: "duck_cleaner" }
     ],
-    ticketMessage: (tipo) => "obrigado(a) por criar um ticket, em breve algum staff te ajudara",
+    ticketMessage: (tipo) => `obrigado(a) por criar um ticket, em breve algum staff te ajudara`,
     embedImage: "https://i.postimg.cc/pVzDGPGt/imagem-2025-10-25-183004200.png"
   },
   {
     id: "1431298408362152060",
-    title: "Membros Discord",
-    embedDesc: "200x: 1.00€\n100x: 0.50€",
+    title: "Membros discord",
+    embedDesc: `precos:
+Por 200x  ➟ 1.00€
+Por 100x  ➟ 0.50€
+Outras quantidades  ➟ Negociado em ticket`,
     options: [
-      { label: "200 Membros", description: "1.00€", value: "membros_200" },
-      { label: "100 Membros", description: "0.50€", value: "membros_100" }
+    { label: "Por 200x  ➟ 1.00€", description: "1.00€", value: "membros_200x" },
+    { label: "Por 100x  ➟ 0.50€", description: "0.50€", value: "membros_100x" },
+    { label: "Outras quantidades", description: "Negociado em ticket", value: "membros_outras" }
     ],
-    ticketMessage: (tipo) => "obrigado(a) por criar um ticket, em breve algum staff te ajudara"
+    ticketMessage: (tipo) => `obrigado(a) por criar um ticket, em breve algum staff te ajudara`
   },
 {
     id: "1456982867799445751",
