30b48164cf38f7d9301e342848a8a9c656974ef5
arte-trimbauser <arteompotrimba71@gmail.com>
2026-02-22 23:26:34 +0000

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,9 +1,14 @@
 module.exports = {
   STAFF_ROLES: ["1393658313006383176","1421595512477450373"],
+
   STAFF_MEMBERS: [
     { label: "Jordan (Costa)", id: "924344854232834068" },
     { label: "Arteex", id: "996454465555136675" }
   ],
+
   DEV_IDS: ["996454465555136675"],
+
   CATEGORY_NAME: "Tickets",
+
+  TRANSCRIPT_LOG_CHANNEL_ID: "1424461544317517854"
 };
