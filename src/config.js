faf5d250a2404eb87a8686fac87791c8425db060
arte-trimbauser <arteompotrimba71@gmail.com>
2026-02-22 18:40:05 +0000

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  STAFF_ROLES: ["1393658313006383176","1421595512477450373"],
+  STAFF_MEMBERS: [
+    { label: "Jordan (Costa)", id: "924344854232834068" },
+    { label: "Arteex", id: "996454465555136675" }
+  ],
+  DEV_IDS: ["996454465555136675"],
+  CATEGORY_NAME: "Tickets",
+};
