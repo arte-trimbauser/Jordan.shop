23286228023271d03f6f210d1bd2c577d56e61b7
arte-trimbauser <arteompotrimba71@gmail.com>
2026-03-12 18:08:45 +0000

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  // Todos os cargos que podem ver tickets e aparecer na lista de chamada
   STAFF_ROLES: [
     "1393658593131233421", // Owner
     "1447241549489639661", // Developer
@@ -8,10 +7,11 @@ module.exports = {
     "1393658313006383176"  // Staff
   ],
 
-  // IDs dos donos/devs para ignorar cooldowns de teste
   DEV_IDS: ["996454465555136675", "924344854232834068"],
 
-  CATEGORY_NAME: "Tickets",
+  // ADICIONA ESTA LINHA COM O ID REAL DA TUA CATEGORIA
+  CATEGORY_ID: "1393629457599828040", 
 
+  CATEGORY_NAME: "Tickets",
   TRANSCRIPT_LOG_CHANNEL_ID: "1424461544317517854"
 };
